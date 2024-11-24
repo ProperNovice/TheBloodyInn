4324978000af1ebed22ced9a9fb0287d2b4af783
ProperNovice <ProperNovice@users.noreply.github.com>
2024-11-24 09:11:12 +0200

    q

---
+++ src/cards/Guest.java
@@ -0,0 +1,15 @@
+package cards;
+
+import utils.Interfaces.IImageViewAble;
+
+public class Guest implements IImageViewAble {
+
+	public Guest() {
+
+	}
+
+	private void createImageView() {
+
+	}
+
+}
